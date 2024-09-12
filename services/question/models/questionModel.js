e9cb3a75592dc99112179eabda2e2506fdc6c717
Samuel Lim <samuelim01@gmail.com>
2024-09-12 23:08:20 +0800

    Tighten question schema

--- services/question/models/questionModel.js
+++ services/question/models/questionModel.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const questionSchema = new mongoose.Schema({
-    id: { type: Number, unique: true },
+    id: { type: Number, required: true, unique: true },
     title: String,
     description: String,
     categories: [String],
-    complexity: String,
+    complexity: { type: String, enum: ['Easy', 'Medium', 'Difficult'] },
 }, { versionKey: false });
 
 const Question = mongoose.model('Question', questionSchema);

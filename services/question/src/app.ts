88dc1fc284740cfc0761649bf978cd8e918ec9a7
Samuel Lim <samuelim01@gmail.com>
2024-09-13 10:44:44 +0800

    Add middleware

--- services/question/src/app.ts
+++ services/question/src/app.ts
@@ -1,9 +1,13 @@
 import express, { Express } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 import router from './routes';
 import questionRouter from './routes/questionRoutes';
 import { syncQuestions } from './setup';
 import { connectDb } from './models';
 import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import { corsOptions } from './config';
 
 const app: Express = express();
 
@@ -12,6 +16,12 @@ connectDb();
 mongoose.connection.once('open', async () => await syncQuestions());
 
 // Middleware
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(cors(corsOptions));
 
 // Routes
 app.use('/', router);
--- services/question/src/config/index.ts
+++ services/question/src/config/index.ts
@@ -3,3 +3,9 @@ export const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
 export const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
 export const MONGODB_URL = 'mongodb://question-db:27017';
 export const MONGODB_DATABASE = 'question';
+
+export const corsOptions = {
+    origin: process.env.corsOrigin ?? true,
+    methods: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+    allowedHeaders: ['Origin', 'X-Request-With', 'Content-Type', 'Accept', 'Authorization']
+}

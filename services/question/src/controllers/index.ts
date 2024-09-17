6cf62984ae28d246573d233ad0348c45cbc3f5b5
Samuel Lim <samuelim01@gmail.com>
2024-09-17 17:13:51 +0800

    Fix getHealth fuction parameters

--- services/question/src/controllers/index.ts
+++ services/question/src/controllers/index.ts
@@ -1,8 +1,7 @@
-import { Response } from 'express';
+import { Request, Response } from 'express';
 
-export const getHealth = async (res: Response) => {
+export const getHealth = async (req: Request, res: Response) => {
     res.status(200).json({
         message: 'Server is up and running!',
     });
-    return;
 };

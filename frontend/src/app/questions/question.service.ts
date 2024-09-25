20205c6a5afc821cccc3d81054a980ad5975d804
limcaaarl <carljovi22@gmail.com>
2024-09-25 22:51:01 +0800

    Add config file to store base url

--- frontend/src/app/questions/question.service.ts
+++ frontend/src/app/questions/question.service.ts
@@ -1,9 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Question } from './question.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class QuestionService {
     constructor(private http: HttpClient) {}
+
+    getList(): Observable<Question> {
+        return this.http.get<Question>('https://lhp-command-center-webapi-dev.azurewebsites.net/api/sites/List');
+    }
 }

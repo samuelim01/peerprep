4686dc3930e5bdd89f641f509b5b60ba3420241e
limcaaarl <carljovi22@gmail.com>
2024-10-03 16:36:02 +0800

    Add codemirror

--- frontend/src/app/collaboration/editor/editor.component.ts
+++ frontend/src/app/collaboration/editor/editor.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Inject, ViewChild } from '@angular/core';
+import { oneDark } from '@codemirror/theme-one-dark';
+import { EditorState, Extension } from '@codemirror/state';
+import { basicSetup } from 'codemirror';
+import { DOCUMENT } from '@angular/common';
+import { EditorView } from 'codemirror';
+import { python } from '@codemirror/lang-python';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
 
 @Component({
     selector: 'app-editor',
     standalone: true,
-    imports: [],
+    imports: [ScrollPanelModule],
     templateUrl: './editor.component.html',
     styleUrl: './editor.component.css',
 })
-export class EditorComponent {}
+export class EditorComponent implements AfterViewInit {
+    @ViewChild('editor') editor!: ElementRef;
+
+    constructor(@Inject(DOCUMENT) private document: Document) {}
+
+    ngAfterViewInit() {
+        const customTheme = EditorView.theme(
+            {
+                '&': {
+                    backgroundColor: 'var(--surface-section)',
+                },
+                '.cm-gutters': {
+                    backgroundColor: 'var(--surface-section)',
+                },
+            },
+            { dark: true },
+        );
+
+        const editorElement = this.editor.nativeElement;
+        const myExt: Extension = [basicSetup, python(), customTheme, oneDark];
+        let state!: EditorState;
+
+        try {
+            state = EditorState.create({
+                doc: '# type your code here',
+                extensions: myExt,
+            });
+        } catch (e) {
+            console.log(e);
+        }
+
+        //console.log(state);
+
+        new EditorView({
+            state,
+            parent: editorElement,
+        });
+    }
+}

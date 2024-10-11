6f8ab95bbecc8b1bcef925ac854637e85c9b6b7e
samuelim01 <61283948+samuelim01@users.noreply.github.com>
2024-10-11 12:40:00 +0800

    Initialize API gateway (#51)
    * Add minimial gateway
    
    * Configure frontend to use api gateway

---
+++ frontend/src/_services/api.service.ts
@@ -0,0 +1,21 @@
+import { API_CONFIG } from '../app/api.config';
+
+/**
+ * Abstract class that serves as a base for API services.
+ */
+export abstract class ApiService {
+    /**
+     * The path for the specific resource, e.g. 'user', 'question', etc.
+     * This property must be implemented by subclasses to specify the
+     * endpoint path for the API resource they represent.
+     */
+    protected abstract apiPath: string;
+
+    /**
+     * Returns the full URL for the API endpoint based on
+     * the specified apiPath.
+     */
+    get apiUrl(): string {
+        return API_CONFIG.baseUrl + this.apiPath;
+    }
+}

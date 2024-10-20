374d8da548113ca2a8726508e82c95ffa4b6f071
limcaaarl <42115432+limcaaarl@users.noreply.github.com>
2024-10-20 23:40:35 +0800

    Nav bar fix (#55)
    * Fix hamburger issues
    
    * Update fonts to store locally
    
    * Revert wrong angular cli version
    
    * Ensure match page requires login
    
    * Fix erlang issues
    
    https://github.com/docker-library/rabbitmq/issues/114
    
    * Fix nav bar css having global effect
    
    ---------
    
    Co-authored-by: Samuel Lim <samuelim01@gmail.com>

--- frontend/src/app/navigation-bar/navigation-bar.component.ts
+++ frontend/src/app/navigation-bar/navigation-bar.component.ts
@@ -1,87 +1,68 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { MenuItem } from 'primeng/api';
 import { CommonModule, NgFor } from '@angular/common';
 import { MenubarModule } from 'primeng/menubar';
 import { AuthenticationService } from '../../_services/authentication.service';
 import { User } from '../../_models/user.model';
+import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
     selector: 'app-navigation-bar',
     standalone: true,
-    imports: [MenubarModule, CommonModule, NgFor],
+    imports: [MenubarModule, CommonModule, NgFor, MenuModule, ButtonModule],
     templateUrl: './navigation-bar.component.html',
     styleUrl: './navigation-bar.component.css',
 })
 export class NavigationBarComponent implements OnInit {
     items: MenuItem[] | undefined;
+    user: User | null = null;
 
-    constructor(
-        private router: Router,
-        private authService: AuthenticationService,
-    ) {}
+    constructor(private authService: AuthenticationService) {}
 
     ngOnInit() {
         this.setMenuItems();
-        this.authService.user$.subscribe(() => this.setMenuItems());
+        this.authService.user$.subscribe(() => {
+            this.setMenuItems();
+            this.user = this.authService.userValue as User;
+        });
     }
 
     setMenuItems() {
         if (this.authService.isLoggedIn) {
-            const user = this.authService.userValue as User;
             this.items = [
                 {
-                    label: user.username,
+                    label: 'Find Match',
+                    icon: 'pi pi-users',
+                    routerLink: '/matching',
+                    class: 'p-submenu-list',
+                },
+                {
+                    label: 'View Questions',
+                    icon: 'pi pi-file',
+                    routerLink: '/questions',
+                    class: 'p-submenu-list',
+                },
+                {
+                    label: 'View Profile',
                     icon: 'pi pi-user',
-                    // route: '/profile',
-                    style: { 'margin-left': 'auto' },
-                    items: [
-                        {
-                            label: 'Find Match',
-                            icon: 'pi pi-users',
-                            route: '/matching',
-                            class: 'p-submenu-list',
-                        },
-                        {
-                            label: 'View Questions',
-                            icon: 'pi pi-file',
-                            route: '/questions',
-                            class: 'p-submenu-list',
-                        },
-                        {
-                            label: 'View Profile',
-                            icon: 'pi pi-user',
-                            // route: '',
-                            class: 'p-submenu-list',
-                        },
-                        {
-                            label: 'View Match History',
-                            icon: 'pi pi-trophy',
-                            // route: '',
-                            class: 'p-submenu-list',
-                        },
-                        {
-                            label: 'Logout',
-                            icon: 'pi pi-sign-out',
-                            class: 'p-submenu-list',
-                            command: () => this.authService.logout(),
-                        },
-                    ],
-                },
-            ];
-        } else {
-            this.items = [
-                {
-                    label: 'Login',
-                    icon: 'pi pi-sign-in',
-                    route: 'account/login',
-                    style: { 'margin-left': 'auto' },
+                    // routerLink: '',
+                    class: 'p-submenu-list',
                 },
                 {
-                    label: 'Sign Up',
-                    icon: 'pi pi-pen-to-square',
-                    route: '/account/register',
-                    class: 'border',
+                    label: 'View Match History',
+                    icon: 'pi pi-trophy',
+                    // routerLink: '',
+                    class: 'p-submenu-list',
+                },
+                {
+                    label: 'Logout',
+                    icon: 'pi pi-sign-out',
+                    class: 'p-submenu-list',
+                    command: () => {
+                        this.authService.logout();
+                        this.user = null;
+                    },
                 },
             ];
         }

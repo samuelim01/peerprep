2ab1535626028e3afd770eb62d23aa62a992bcf6
limcaaarl <carljovi22@gmail.com>
2024-10-29 18:07:14 +0800

    Fix bugs

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -20,7 +20,6 @@ export const routes: Routes = [
     {
         path: 'start',
         component: CollaborationComponent,
-        // canActivate: [AuthGuardService],
         canActivate: [AuthGuardService, CollabGuardService],
     },
     {

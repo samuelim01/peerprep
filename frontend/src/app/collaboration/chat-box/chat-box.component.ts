a0e32f05ee664dcdd553f22be9ecc304a7931d0a
KhoonSun47 <justforwks@gmail.com>
2024-11-01 00:00:21 +0800

    Update CSS and HTML for Chat Service
    (cherry picked from commit 951077c058b482e08df1f2a4f5fbb476a38c926d)

--- frontend/src/app/collaboration/chat-box/chat-box.component.ts
+++ frontend/src/app/collaboration/chat-box/chat-box.component.ts
@@ -1,60 +1,64 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
 import * as Y from 'yjs';
 import { AuthenticationService } from '../../../_services/authentication.service';
 import { WebsocketProvider } from 'y-websocket';
-import { NgForOf } from '@angular/common';
+import {NgClass, NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-chat-box',
   standalone: true,
-  imports: [NgForOf],
+  imports: [NgForOf, NgClass],
   templateUrl: './chat-box.component.html',
   styleUrls: ['./chat-box.component.css'],
 })
-export class ChatBoxComponent implements OnInit, AfterViewInit {
+export class ChatBoxComponent implements AfterViewInit {
   @Input() ydoc!: Y.Doc;
   @Input() wsProvider!: WebsocketProvider;
   @Input() roomId!: string;
 
   @ViewChild('chatInput') chatInput!: ElementRef;
+  @ViewChild('messagesContainer') messagesContainer!: ElementRef;
 
-  messages: string[] = [];
-  yChatArray!: Y.Array<string>;
-  userId!: string;
+  messages: { text: string; sender: string }[] = [];
+  yChatArray!: Y.Array<{ text: string; sender: string }>;
+  username!: string;
 
   constructor(private authService: AuthenticationService) {}
 
-  ngOnInit(): void {
-  }
-
   ngAfterViewInit() {
-    this.getUserId();
+    this.getUsername();
     this.initYdoc();
     this.initArrayListener();
   }
 
-  getUserId() {
-    this.userId = this.authService.userValue?.id || 'Guest';
+  getUsername() {
+    this.username = this.authService.userValue?.username || 'Guest';
   }
 
   initYdoc() {
-    this.yChatArray = this.ydoc.getArray<string>('chatMessages');
+    this.yChatArray = this.ydoc.getArray('chatMessages');
   }
 
   initArrayListener() {
     this.yChatArray.observe(() => {
       this.messages = this.yChatArray.toArray();
+      this.scrollToBottom();
     });
   }
 
   sendMessage() {
     const message = this.chatInput.nativeElement.value.trim();
     if (message) {
-      const formattedMessage = `${this.userId}: ${message}`;
-
-      this.yChatArray.push([formattedMessage]);
-
+      const newMessage = { text: message, sender: this.username };
+      this.yChatArray.push([newMessage]);
       this.chatInput.nativeElement.value = '';
+      this.scrollToBottom();
     }
+  }
+
+  scrollToBottom() {
+    setTimeout(() => {
+      this.messagesContainer.nativeElement.scrollTop = this.messagesContainer.nativeElement.scrollHeight;
+    }, 100);
   }
 }

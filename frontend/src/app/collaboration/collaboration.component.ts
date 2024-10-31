07d35138aa8282e2ebcc3e54fd5ef430944ce1c0
limcaaarl <carljovi22@gmail.com>
2024-10-31 11:02:21 +0800

    Initialise chat component
    (cherry picked from commit 82143f3d52ea8ccad520893dd983cd09e578be32)

--- frontend/src/app/collaboration/collaboration.component.ts
+++ frontend/src/app/collaboration/collaboration.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { QuestionBoxComponent } from './question-box/question-box.component';
 import { EditorComponent } from './editor/editor.component';
 import { SplitterModule } from 'primeng/splitter';
+import { ChatBoxComponent } from './chat-box/chat-box.component';
 
 @Component({
     selector: 'app-collaboration',
     standalone: true,
-    imports: [QuestionBoxComponent, EditorComponent, SplitterModule],
+    imports: [QuestionBoxComponent, EditorComponent, SplitterModule, ChatBoxComponent],
     templateUrl: './collaboration.component.html',
     styleUrl: './collaboration.component.css',
 })

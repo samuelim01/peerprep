af10ecb90f9cea69099d67ba21c930976a66edee
KhoonSun47 <justforwks@gmail.com>
2024-11-01 18:55:22 +0800

    Update Chat Service - Added Mute/Unmute Functionality
    (cherry picked from commit 807485f65b856084d71d8be92d0b506e0148995e)

--- frontend/src/app/collaboration/chat-box/chat-box.component.ts
+++ frontend/src/app/collaboration/chat-box/chat-box.component.ts
@@ -1,8 +1,8 @@
-import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
 import * as Y from 'yjs';
-import { AuthenticationService } from '../../../_services/authentication.service';
-import { WebsocketProvider } from 'y-websocket';
-import { NgClass, NgForOf } from '@angular/common';
+import {AuthenticationService} from '../../../_services/authentication.service';
+import {WebsocketProvider} from 'y-websocket';
+import {NgClass, NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-chat-box',
@@ -21,13 +21,16 @@ export class ChatBoxComponent implements AfterViewInit {
 
   messages: { text: string; sender: string; timestamp: string }[] = [];
   yChatArray!: Y.Array<{ text: string; sender: string; timestamp: string }>;
+  yMute!: Y.Map<boolean>;
   username!: string;
+  isMuted = false;
 
   constructor(private authService: AuthenticationService) {}
 
   ngAfterViewInit() {
     this.getUsername();
     this.initYdoc();
+    this.initYMap();
     this.initArrayListener();
   }
 
@@ -39,14 +42,31 @@ export class ChatBoxComponent implements AfterViewInit {
     this.yChatArray = this.ydoc.getArray('chatMessages');
   }
 
+  initYMap() {
+    this.yMute = this.ydoc.getMap('muteStatus');
+
+    if (!this.yMute.has(this.username)) {
+      this.yMute.set(this.username, false);
+    }
+
+    this.yMute.observe(event => {
+      this.isMuted = this.yMute.get(this.username) ?? false;
+    });
+  }
+
   initArrayListener() {
     this.yChatArray.observe(() => {
-      this.messages = this.yChatArray.toArray();
+      const allMessages = this.yChatArray.toArray();
+      this.messages = allMessages.filter(message => {
+        if (message.sender === this.username || message.sender === 'System') return true;
+        return !this.isMuted;
+      });
       this.scrollToBottom();
     });
   }
 
   sendMessage() {
+    if (this.isMuted) return;
     const message = this.chatInput.nativeElement.value.trim();
     if (message) {
       const timestamp = new Date().toLocaleString('en-SG', {
@@ -62,6 +82,34 @@ export class ChatBoxComponent implements AfterViewInit {
       this.chatInput.nativeElement.value = '';
       this.scrollToBottom();
     }
+  }
+
+  toggleMute() {
+    const timestamp = new Date().toLocaleString('en-SG', {
+      day: 'numeric',
+      month: 'numeric',
+      year: '2-digit',
+      hour: 'numeric',
+      minute: '2-digit',
+      hour12: true,
+    });
+    this.isMuted = !this.isMuted;
+    this.yMute.set(this.username, this.isMuted);
+
+    if (this.isMuted) {
+      this.sendSystemMessage(`${this.username} has muted the conversation.`, timestamp);
+    } else {
+      this.sendSystemMessage(`${this.username} has un-muted the conversation.`, timestamp);
+    }
+  }
+
+  sendSystemMessage(text: string, timestamp: string) {
+    const systemMessage = {
+      text,
+      sender: 'System',
+      timestamp,
+    };
+    this.yChatArray.push([systemMessage]);
   }
 
   scrollToBottom() {

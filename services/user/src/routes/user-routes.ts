b4ee292c29e82259373bb200ba1501e89ff20e92
McNaBry <mcnabry123@gmail.com>
2024-11-13 01:18:51 +0800

    Add new routes to user service to handle update to user details
    * Redefine zod schemas for better reusability
    * Add route to handle update to user's username and email
    * Add route to handle update to user's password

--- services/user/src/routes/user-routes.ts
+++ services/user/src/routes/user-routes.ts
@@ -5,7 +5,9 @@ import {
     deleteUser,
     getAllUsers,
     getUser,
+    updatePassword,
     updateUser,
+    updateUsernameAndEmail,
     updateUserPrivilege,
 } from '../controller/user-controller';
 import { verifyAccessToken, verifyIsAdmin, verifyIsOwnerOrAdmin } from '../middleware/basic-access-control';
@@ -21,6 +23,10 @@ router.post('/', createUser);
 router.get('/:id', verifyAccessToken, verifyIsOwnerOrAdmin, getUser);
 
 router.patch('/:id', verifyAccessToken, verifyIsOwnerOrAdmin, updateUser);
+
+router.patch('/username-email/:id', verifyAccessToken, verifyIsOwnerOrAdmin, updateUsernameAndEmail);
+
+router.patch('/password/:id', verifyAccessToken, verifyIsOwnerOrAdmin, updatePassword);
 
 router.delete('/:id', verifyAccessToken, verifyIsOwnerOrAdmin, deleteUser);
 
